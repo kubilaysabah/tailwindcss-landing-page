bb82c83478c11b44faa45891cd656a247fea2ca3
Kubilay <kubilaysabah@gmail.com>
2022-11-25 00:47:03 +0300

    add eslitn and chartjs

--- src/index.js
+++ src/index.js
@@ -1,30 +1,32 @@
+import Chart from 'chart.js/auto'
+
 const loadChart = () => {
-  const ctx = document.querySelector("[chart]");
+	const ctx = document.querySelector('[chart]')
 
-  const data = {
-    labels: ["Direct", "Refferal", "Search", "Social", "Paid"],
-    datasets: [
-      {
-        label: "My First Dataset",
-        data: [51, 12.5, 12.5, 12.5, 12.5],
-        backgroundColor: [
-          "#AEA3E6",
-          "#FFDA62",
-          "#BCECFF",
-          "#FDAFA6",
-          "#C9FDA1",
-        ],
-        hoverOffset: 4,
-      },
-    ],
-  };
+	const data = {
+		labels: ['Direct', 'Refferal', 'Search', 'Social', 'Paid'],
+		datasets: [
+			{
+				label: 'My First Dataset',
+				data: [51, 12.5, 12.5, 12.5, 12.5],
+				backgroundColor: [
+					'#AEA3E6',
+					'#FFDA62',
+					'#BCECFF',
+					'#FDAFA6',
+					'#C9FDA1',
+				],
+				hoverOffset: 4,
+			},
+		],
+	}
 
-  new Chart(ctx, {
-    type: "doughnut",
-    data: data,
-  });
-};
+	new Chart(ctx, {
+		type: 'doughnut',
+		data: data,
+	})
+}
 
-window.addEventListener("load", () => {
-  loadChart();
-});
+window.addEventListener('load', () => {
+	loadChart()
+})
